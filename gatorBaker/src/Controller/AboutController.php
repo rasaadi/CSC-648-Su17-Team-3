4383843ae6cf8f5b61cae60dc4a36b8bf4c0c674
Rafsan Saadi <rsaadi@mail.sfsu.edu>
2017-06-22 03:00:06 -0700

    Added AboutController

---
+++ gatorBaker/src/Controller/AboutController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafsan
+ * Date: 6/22/2017
+ * Time: 2:14 AM
+ */
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+
+class AboutController extends AppController
+{
+    public function aboutUs(){
+
+    }
+
+
+}
+
+
