413e494ed3e29a136596c6beee27338503ad1877
JYang25 <jyang15@mail.sfsu.edu>
2017-07-14 15:18:26 -0700

    Update HomeController.php

--- gatorBaker/src/Controller/HomeController.php
+++ gatorBaker/src/Controller/HomeController.php
@@ -4,8 +4,43 @@ namespace App\Controller;
 use App\Controller\AppController;
 class HomeController extends AppController
 {
-    public function index()
-    {
-    }
+    <?php
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+use Cake\Datasource\ConnectionManager;
+
+class SearchIDController extends AppController{
+	
+	public function index(){
+/*
+		echo "this is from controller<br/>";
+		print_r($this->request->getData());
+		echo "<br/>";
+*/
+		$in =  $this->request->getData('theTitle');
+//		echo "$in"."<br/>";
+/*
+		echo "this is from controller2<br/>";
+		$this->set('newvalue',$this->request->getData('username'));
+		$this->set('newarray',$this->request->getData());
+*/
+
+
+	$connection = ConnectionManager::get('default');
+	$results = $connection->execute("SELECT * FROM media_info where title=('$in')")->fetchAll('assoc');
+
+
+
+		$this->set('res',$results);
+
+
+
+	}
+}
+
     
 }
