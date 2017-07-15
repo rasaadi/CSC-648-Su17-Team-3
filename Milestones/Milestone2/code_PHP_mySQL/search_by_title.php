441ade0afa09972c93f5dabe960b363515621e30
JYang25 <jyang15@mail.sfsu.edu>
2017-07-14 18:48:22 -0700

    Delete search_by_title.php

--- Milestones/Milestone2/code_PHP_mySQL/search_by_title.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function search_by_title($str,$conn){ 
-		$sql = "SELECT * FROM image where title=('$str')";
-//		$sql = "SELECT * FROM image where title='city'";
-		$result = $conn->query($sql);
-		return $result;  //returns the second argument passed into the function
-	}
-?>
