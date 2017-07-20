9fe26b6657f3db69790977fcfd9265fc242fde3f
JYang25 <jyang15@mail.sfsu.edu>
2017-07-20 11:55:06 -0700

    Update SearchIDController.php

--- gatorBaker/src/Controller/SearchIDController.php
+++ gatorBaker/src/Controller/SearchIDController.php
@@ -19,7 +19,28 @@ class SearchIDController extends AppController{
 
 
         $in =  $this->request->getData('theTitle');
+        $field = $this->request->getData('field');
 	    $connection = ConnectionManager::get('default');
+        echo "$in"."<br/>";
+		echo "$field"."<br/>";
+		if(empty($field)){
+			echo "it is empty <br/>";
+            $results = $connection->execute("SELECT * FROM media_info where title LIKE '%".$in."%' OR media_cat LIKE '%".$in."%' OR owner LIKE '%".$in."%'")->fetchAll('assoc');
+		} elseif($field=='Cars'){
+			echo "it is Cars <br/>";
+            $results = $connection->execute("SELECT * FROM media_info where title LIKE '%".$in."%' AND media_cat='cat' ")->fetchAll('assoc');
+		} elseif($field=='Cities'){
+			echo "it is Cities <br/>";
+		} elseif($field=='People'){
+			echo "it is People <br/>";
+		} elseif($field=='Animal'){
+			echo "it is Animal <br/>";
+		} elseif($field=='Animal'){
+			echo "it is LandScape <br/>";
+		} else{
+			echo "error!<br/>";
+		}
+
 	    $results = $connection->execute("SELECT * FROM media_info where title LIKE '%".$in."%' OR media_cat LIKE '%".$in."%' OR owner LIKE '%".$in."%'")->fetchAll('assoc');
         $this->set('res',$results);
 
