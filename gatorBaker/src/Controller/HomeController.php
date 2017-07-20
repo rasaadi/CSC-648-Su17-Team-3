ff939c9e8720bf87ba5db5b493c5ab63783d848b
JYang25 <jyang15@mail.sfsu.edu>
2017-07-20 08:52:01 -0700

    Update HomeController.php

--- gatorBaker/src/Controller/HomeController.php
+++ gatorBaker/src/Controller/HomeController.php
@@ -8,27 +8,11 @@ class HomeController extends AppController
 {
     public function index()
     {
-        /*
-                echo "this is from controller<br/>";
-                print_r($this->request->getData());
-                echo "<br/>";
-        */
+
         $in = $this->request->getData('theTitle');
-//		echo "$in"."<br/>";
-        /*
-                echo "this is from controller2<br/>";
-                $this->set('newvalue',$this->request->getData('username'));
-                $this->set('newarray',$this->request->getData());
-        */
-
-
         $connection = ConnectionManager::get('default');
-//	    $results = $connection->execute("SELECT * FROM media_info where title=('$in')")->fetchAll('assoc');
-//        $results = $connection->find("all",array('condition' => array('title LIKE' => ('$in'))));
-        print_r($results);
 
-        $this->set('res', $results);
-
+/*
         //create users table
         $createTable = $this->table('users');
         $createTable
@@ -80,7 +64,7 @@ class HomeController extends AppController
             $errorMessage = 'You need to same password in comfirm';
         }
 
-
+*/
 
 
     }
