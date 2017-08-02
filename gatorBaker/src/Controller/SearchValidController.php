d65030dee3a8890f26152949eb9e38a11029805d
BrandonSYu <beyu94@gmail.com>
2017-08-01 21:53:46 -0700

    Update SearchValidController.php

--- gatorBaker/src/Controller/SearchValidController.php
+++ gatorBaker/src/Controller/SearchValidController.php
@@ -5,4 +5,10 @@ use Cake\Event\Event;
 
 class SearchValidController extends AppController
 {
+    $errors = array();
+    if(!$this->SearchValidTable->$validates($this->data) {
+        $errors = $this->SearchValidTable->invalidFields();
+        $this->Session->save('SearchValidTable.invalid',$errors);
+        $this->redirect('/plans');
 }
+?>
