15a943c99755b9e6e4cb41734509aab3de6c727a
JYang25 <jyang15@mail.sfsu.edu>
2017-08-03 01:04:37 -0700

    Add files via upload
    add the profile controller

---
+++ gatorBaker/src/Controller/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+use Cake\Datasource\ConnectionManager;
+class ProfileController extends AppController
+{
+
+    public function index()
+    {
+    
+	}
+}
+?>
+
+
