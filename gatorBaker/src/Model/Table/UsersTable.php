3954c6c640979ea980902b706ad2c4c4d9df294f
TakahiroOdaka <todaka@mail.sfsu.edu>
2017-08-06 07:49:14 +0900

    Add agrrement check boxmon register page and make the confirm password field invisible

--- gatorBaker/src/Model/Table/UsersTable.php
+++ gatorBaker/src/Model/Table/UsersTable.php
@@ -8,11 +8,42 @@ use Cake\Validation\Validator;
 class UsersTable extends Table
 {
 
+
+
     public function validationDefault(Validator $validator)
     {
         return $validator
             ->notEmpty('password', 'A password is required')
-            ->notEmpty('email', 'An email is required');
+            ->add('password', [
+                'length' => [
+                    'rule' => ['lengthBetween', 4,15],
+                    'message' => 'Password need to be between 4 to 15 characters',
+                ]
+            ])
+            ->add('password', [
+                'compare' => [
+                    'rule' => ['compareWith', 'confirm_password'],
+                    'message' => 'Comfirm password should be same password',
+                ]
+            ])
+
+            ->notEmpty('email', 'An email is required')
+            ->notEmpty('confirm_password', 'You need to input password again')
+            ->add('confirm_password', [
+                'length' => [
+                    'rule' => ['lengthBetween', 4,15],
+                    'message' => 'Password need to be between 4 to 15 characters',
+                ]
+            ])
+            ->requirePresence('accept_terms')
+            ->notEmpty('accept_terms', 'You must agree to the terms', 'create');
+
     }
+
+
+
+
+
+
 
 }
